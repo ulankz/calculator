632b681db9a19d5ffa5814addc14c477939e5ee8
Ulan Assanov <Ulan@users-iMac-2.local>
2017-10-31 19:29:20 +0600

    adds functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,5 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBOutlet weak var output: UITextView!
+    @IBAction func touchDigit(_ sender : UIButton){
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrrentlyInDisplay = output!.text!;
+            output.text = textCurrrentlyInDisplay + digit;
+        }
+        else{
+            output.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    @IBAction func clear(_ sender : UIButton){
+        output.text.removeAll();
+    }
+    func distance(from startX:Int, to endX:Int,using color:CGColor) -> Int{
+        return endX - startX;
+    }
+    @IBAction func signChange(_ sender : UIButton){
+        var digit:Int
+        guard let text = output.text,
+            !text.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).isEmpty else {
+                    return
+        }
+        digit = Int(text)!
+        output.text = String(digit * (-1))
+    }
+    @IBAction func performOperation(_ sender : UIButton){
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol {
+            case "":
+                output.text = String(Double.pi)
+            default:
+                break
+            }
+        }
+    }
 }
 
