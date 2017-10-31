b9a687b19d6d11624b1697b4df61839ddb25b5ba
Ulan Assanov <Ulan@users-iMac-2.local>
2017-10-31 16:22:59 +0600

    Initial Commit

---
+++ Calculator/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by Ulan Assanov on 10/31/17.
+//  Copyright © 2017 Ulan Assanov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ulan Assanov on 10/31/17.
+//  Copyright © 2017 Ulan Assanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
